02bf515e1222d9eabbb8fb1093fc71079dbe1917
Akshaya-jangam <ajakshaya2003@gmail.com>
2026-03-03 18:52:05 +0530

    a

--- server/index.js
+++ server/index.js
@@ -12,12 +12,12 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
 
-app.use(cors());
+app.use(cors({ origin: "*"}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -92,6 +92,9 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log("Server running on port 5000 😤🔥");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 😤🔥`);
+
 });
